3986e4898281fa9904505a98ddaeb4a98199dbf4
Elihu Cruz <eaca230596@gmail.com>
2022-06-20 18:52:22 -0500

    chore: testing db update

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,33 +1,38 @@
-
+use config::{Config, ConfigError, File};
 use serde::Deserialize;
-use config::{File, Config, ConfigError};
 
 #[derive(Deserialize)]
 pub struct Settings {
-  pub database: DatabaseSettings,
-  pub application_port: u16
+    pub database: DatabaseSettings,
+    pub application_port: u16,
 }
 
 #[derive(Deserialize)]
 pub struct DatabaseSettings {
-  pub username: String,
-  pub password: String,
-  pub port: u16,
-  pub host: String,
-  pub database_name: String
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
 }
 
 impl DatabaseSettings {
-  pub fn connection_string(&self) -> String {
-    format!(
-      "postgres://{}:{}@{}:{}/{}",
-      self.username, self.password, self.host, self.port, self.database_name
-    )
-  }
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
 }
 
-pub fn get_configuration () -> Result<Settings, ConfigError> {
-  let mut settings = Config::default();
-  settings.merge(File::with_name("configuration"))?;
-  settings.try_into()
+pub fn get_configuration() -> Result<Settings, ConfigError> {
+    let mut settings = Config::default();
+    settings.merge(File::with_name("configuration"))?;
+    settings.try_into()
 }
